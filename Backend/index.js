5c764c0852d7ef74414acc435f4530cb0381c10a
kavithma <kavithmadilmanie@gmail.com>
2024-11-15 21:43:15 +0530

    gitgnores

---
+++ Backend/index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+//express app
+const app = express();
+const router = express.Router();
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+})
+
+app.use(cors());
+
+const corsOptions = {
+  origin: "https://localhost:3000/", // frontend URI (ReactJS)
+}
+
+
+
+//routes
+
+
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI || 2000)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+
+      console.log('connected to db and listening on port', process.env.PORT);
+    })
+
+  })
+  .catch((error) => {
+
+    console.log(error);
+  })
+
+
+
