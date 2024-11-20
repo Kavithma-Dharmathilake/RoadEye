a617f95405d340436aa3115d35532157bdbc2ab1
dyonwalter <135725036+dyonwalter@users.noreply.github.com>
2024-11-20 18:25:54 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 //express app
 const app = express();
@@ -14,42 +12,27 @@ app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
-})
+});
 
 app.use(cors());
 
-
-
-
 const corsOptions = {
   origin: "https://localhost:3000/", // frontend URI (ReactJS)
-}
-
+};
 
 // Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/protected', require('./routes/protectedRoutes'));
-
-
-
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/protected", require("./routes/protectedRoutes"));
 
 //connect to db
-mongoose.connect(process.env.MONGO_URI || 2000)
+mongoose
+  .connect(process.env.MONGO_URI || 2000)
   .then(() => {
     //listen for requests
     app.listen(process.env.PORT, () => {
-
-      console.log('connected to db and listening on port', process.env.PORT);
-    })
-
+      console.log("connected to db and listening on port", process.env.PORT);
+    });
   })
   .catch((error) => {
-
     console.log(error);
-  })
-
-
-
-
-
-  
+  });
